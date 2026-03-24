7dd84d37c1bfcf5ae341397bb1fca4347bc50d02
Alena <alenkra0@gmail.com>
2026-03-24 16:09:58 +0100

    Fix GitHub Pages deployment path and routing

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import './App.css'
 
 function App() {
   return (
-    <Router>
+    <Router basename={import.meta.env.BASE_URL}>
       <Navbar />
       <Routes>
         <Route path="/" element={<Main />} />
